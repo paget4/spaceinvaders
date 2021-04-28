b1a4812efdcf69ed8f3a136ba875019472d6e38c
Maxime <Maxime.paget26@gmail.com>
2021-04-28 12:02:22 +0200

    tp2

---
+++ src/main/java/fr/unilim/spaceinvaders/utils/HorsEspaceJeuException.java
@@ -0,0 +1,10 @@
+package fr.unilim.spaceinvaders.utils;
+
+@SuppressWarnings("serial")
+public class HorsEspaceJeuException extends RuntimeException {
+
+    public HorsEspaceJeuException(String message) {
+	    super(message);
+    }
+
+}
